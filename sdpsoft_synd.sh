d908f612930fa24069ae4efc83f5f6b4fdfe727c
Stig Oskar Myhre Ofstad <stoo@statoil.com>
2018-03-13 13:59:51 +0000

    Add new file

---
+++ sdpsoft_synd.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE:  sync.sh
+#
+#         USAGE:  sync.sh [-a, --all] [-f, --files] [--only-env-files]"
+#
+#   DESCRIPTION:  Uses rsync to push and syncronize local sdpsoft directory
+#                 to all remote RGS Statoil servers.
+#
+#       OPTIONS:  -a --all -f --files --only-env-files
+#  REQUIREMENTS:  run as spdadm on vmm03.prod.sdp.ststoil.no in /data/sdpsoft
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Stig O. M. Ofstad, stoo@statoil.com
+#       COMPANY:  Statoil
+#       VERSION:  1.0
+#       CREATED:  ti. 13. mars 12:32:27 +0100 2018
+#      REVISION:  ---
+#===============================================================================
+PROGRAMNAME=$(basename $0)
+cd $(dirname $0)
+SDPSOFT_REMOTE_DIR="/prog/sdpsoft/"
+SERVERS=(
+    tr-vsdp02.tr.statoil.no
+    st-vcris01.st.statoil.no
+    be-linrgsn002.be.statoil.no
+    hou-linrgsn034.hou.statoil.no
+    rio-linrgsn003.rio.statoil.no
+    cal-linrgsn009.cal.statoil.no
+    stjohn-linrgs006.stjohn.statoil.no
+    ha-linrgsn050.ha.statoil.no
+    st-lcmtop01.st.statoil.no
+    hou-lcctop01.hou.statoil.no
+    ffs01.hou.statoil.no
+)
+
+# Print usage info if none or an invalid argument is given.
+function usage {
+    echo "usage: $PROGRAMNAME [-a, --all] [-i, --include] [--only-env-files]"
+    echo -e "      -f, --files           Only sync these files. Example: \"./sync.sh --files python2.7.14 tmux-10.7\""
+    echo "      -a, --all             Sync all files"
+    echo "      --only-env-files      Only sync 'the env.sh' and 'env.csh' files"
+    echo "   requires atleast one argument"
+    exit 1
+}
+
+if [ "$1" = "--only-env-files" ]; then
+    for server in ${SERVERS[@]}; do
+        echo " SYNCING to $server"
+        rsync -va --include="env.sh" --include="env.csh" --exclude="*" . $server:$SDPSOFT_REMOTE_DIR
+    done
+elif [ "$1" = "-a" ] || [ "$1" = "--all" ]; then
+    for server in ${SERVERS[@]}; do
+        echo " SYNCING to $server"
+        # Sync the updated environment-files last to avoid putting the updated
+        # software in limbo while the new version is syncing
+        rsync -va --exclude-from=".gitignore" --exclude="env.sh" --exclude="env.csh" . $server:$SDPSOFT_REMOTE_DIR
+        rsync -va --include="env.sh" --include="env.csh" --exclude="*" . $server:$SDPSOFT_REMOTE_DIR
+    done
+elif [ "$1" = "-f" ] || [ "$1" = "--files" ]; then
+    # Shift the $@ parameters. $2 becomes $1. To omit '--files'.
+    shift 1
+    for server in ${SERVERS[@]}; do
+        echo " SYNCING to $server"
+        for file in "$@"; do
+            rsync -va $file $server:$SDPSOFT_REMOTE_DIR
+        done
+    done
+else
+    usage
+fi
