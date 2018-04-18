794b241db473b5a1008f0c2aa0c8b0ce0d6e6e9d
Stig Oskar Myhre Ofstad (IT SI SIT) <stoo@lt-lx947113.client.statoil.net>
2018-04-18 12:42:29 +0200

    Only check the last log per day.

--- sensu/sensu_backup_errors.sh
+++ sensu/sensu_backup_errors.sh
@@ -5,24 +5,27 @@ LOG_PREFIX="backup_report_"
 TIMESTAMP=$(date +\%y.\%m.\%d_) 
 BACKUP_LOG="$LOG_PATH$LOG_PREFIX$TIMESTAMP*.log"
 # This is perhaps not the best way to do it. An alternative could be to
-# Integrate a solution like this in the backup-sript
+# Integrate a solution like this in the backup-script
 #       if [ $? -ne 0 ]
 #       then
 #       echo "ERROR:"
 #       fi
-if [ ! -f $BACKUP_LOG ]; then
+
+# In case there are several logs from same day, get the last one.
+BACKUP_LOG="$(ls $BACKUP_LOG | cut -d " " -f 1 | tail -n 1)"
+if [ ! -f "$BACKUP_LOG" ]; then
     echo "Could not find the file $BACKUP_LOG. Assuming the backup went OK. "
     exit 0 
 fi
 # If the logfile containes "error", echo the error line with context, and exit with code 2
 if grep -i --quiet "error" $BACKUP_LOG; then
     echo "The backup job completet with error(s)! Error with context:"
-    grep -i --before-context=4 --after-context=2 "error" $BACKUP_LOG
+    grep -i --color=auto --before-context=2 --after-context=1 "error" $BACKUP_LOG
     exit 2
 # Same as above, just with "warning" and exit code 1
 elif grep -i --quiet "warning" $BACKUP_LOG; then
     echo "The backup job completet with warning(s)! Warning with context:"
-    grep -i --before-context=4 --after-context=2 "warning" $BACKUP_LOG
+    grep -i --before-context=1 --after-context=1 "warning" $BACKUP_LOG
     exit 1
 else
     echo "Could not locate the keywords 'warning' or 'error' in the last backup report. Status OK."
