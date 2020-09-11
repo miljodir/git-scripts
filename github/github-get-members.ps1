8e057e3449bf7fc96a6273b3e65c10a4f1099b02
Audun Lønset Solemdal <AULS@equinor.com>
2020-09-11 14:30:53 +0200

    github: get-members do-while

--- github/github-get-members.ps1
+++ github/github-get-members.ps1
@@ -13,17 +13,39 @@ else {
 }
 
 
-$offset = ""
+$after = "after:"
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("content-type","application/json")
 $headers.Add("Authorization","bearer $token")
 
-$bodyA = '{"query":"query { organization(login: \"Equinor\"){ membersWithRole(first: 100) { edges { node {name, login} } pageInfo {endCursor, hasNextPage} } } }"}'
+$bodyA = '{"query":"query { organization(login: \"Equinor\"){ membersWithRole(first: 100 ) { edges { node {name, login} } pageInfo {endCursor, hasNextPage} } } }"}'
 $bodyB = '{"query":"query { organization(login: \"Equinor\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100) { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
 
+$offset = ""
 
-$response = Invoke-WebRequest -Uri $url -Method Post -Body $bodyB -Headers $headers
-Write-host $response.Content
+Do
 
-echo  $response.Content
+{
+    #$bodyA = '{"query":"query { organization(login: \"Equinor\"){ membersWithRole(first: 100 \"\$offset\") { edges { node {name, login} } pageInfo {endCursor, hasNextPage} } } }"}'
+    echo "offset post iteration: $offset"
+    $response = Invoke-WebRequest -Uri $url -Method Post -Body $bodyA -Headers $headers
+
+    $response.content
+    $resp = $response.Content | convertfrom-json
+    $offset = $resp.data.organization.membersWithRole.pageInfo.endCursor
+    $hasNextPage = $resp.data.organization.membersWithRole.pageInfo.hasNextPage
+    $hashtable = $resp.data.organization.membersWithRole.edges
+
+    $bodyA = '{"query":"query { organization(login: \"Equinor\"){ membersWithRole(first: 100 after: \"$offset\") { edges { node {name, login} } pageInfo {endCursor, hasNextPage} } } }"}'
+    $bodyA = $ExecutionContext.InvokeCommand.ExpandString($bodyA)
+
+    echo $hashtable
+    echo "Has next page: $hasNextPage"
+    echo "offset: $offset"
+
+    echo "Page completed. Next iteration: "
+
+} While ($hasNextPage) 
+
+
