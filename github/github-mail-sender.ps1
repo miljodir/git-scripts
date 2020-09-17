b3f71e8e8eb534b30f4517c08908eb181a01fcfa
Audun Lønset Solemdal <AULS@equinor.com>
2020-09-17 13:01:49 +0200

    update mail-sender to reality

--- github/github-mail-sender.ps1
+++ github/github-mail-sender.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Continue"
 
 
 if (Get-InstalledModule -Name ImportExcel) 
@@ -12,14 +12,14 @@ else
 }
 
 $userFrom = "gm_toolbox@equinor.com"
-$excelFile = "C:\github.xlsx"
-$list = Import-Excel -Path $excelFile
+$excelFile = "C:\appl\ghmails2.xlsx"
+$list = Import-Excel -Path $excelFile -WorksheetName "Sheet1"
 
 if ($?)
 
     {
-        $emails = $list.Email
-        $users = $list.Login
+        $users = $list.login
+        $emails = $list.mail
     }
 else
     {
@@ -29,27 +29,30 @@ else
 
 $cred = Get-Credential -username $userFrom
 
-for ($i = 0; $i -lt $orgs.Length; $i++) { 
+for ($i = 0; $i -lt $emails.Length; $i++) { 
 
-    Write-host "Sending email to user: $($emails[$i]) with username: $($users[$i])"
+    Write-host "Sending email to user: $($emails[$i]) with Github username: $($users[$i])"
 
-    Send-MailMessage -From "GM IT Toolbox <gm_toolbox@equinor.com>" -To $emails[$i] -Subject "Info regarding your Github account $($users[$i])" `
-    -Body "
-    You are receiving this email because you are part of Equinor's organization on Github.
+    Send-MailMessage -From "GM IT Toolbox <gm_toolbox@equinor.com>" -To $emails[$i] -Subject "Action required for your Github account $($users[$i])" `
+    -Cc $userFrom -Body "
+
+    You are receiving this email because you are part of Equinor's organization on Github, as we believe the Github account $($users[$i]) https://github.com/$($users[$i]) belongs to you.
+    If this information is incorrect, or you do not use Github anymore, you may safely ignore this email.
 
     Equinor intends to enforce Single Sign-on (SSO) through Azure AD. Your Github account is currently NOT linked with SSO.
 
     We ask you to fix this by:
-    1) Clicking this URL to complete linking to your account: https://github.com/settings/tokens
+    1) Clicking this URL to complete linking to your account: https://github.com/orgs/equinor/sso
     2) Ensure that your existing Personal Access tokens (if any) will continue to work, by clicking 'Enable SSO' per token: https://github.com/settings/tokens
-    Note that if you were not part of the group from before, you may need to wait up to 75 minutes before the latter link will work for you.
 
-    If you are now getting an error during the linking process or during signing in:
+    If you are getting an error during the linking process or during sign-in after linking your account:
     Ensure you are part of the AccessIT group 'Github' https://accessit.equinor.com/Search/Search?term=github
+    You may need to wait up to 75 minutes after AccessIT request approval before the account linking will work.
 
-    Please take a few minutes to do this, as this will save both yourself and the SDP team time down the line.
+    Please take a few minutes to do this, as this will save both yourself and the Toolbox team time down the line. 
+    Your account will eventually be removed from Equinor's Github organization if you do not take action. 
     
-    If you have any questions, either reach out to @toolbox in the #sdpteam channel on equinor.slack.com, or reply to this email" `
+    If you have any questions, either reach out to @toolbox in the #sdpteam channel on https://equinor.slack.com, or reply to this email." `
     -SmtpServer "mrrr.statoil.com" -Port 25 -Credential $cred -UseSsl
 }
 
