02ed3ebf3ca70e4ffc4e35d153087b005c7435ad
audunsolemdal <auls@equinor.com>
2020-11-05 13:21:52 +0100

    github/outside-collabs: working sample, ignore 'all' API call for now

--- github/github-get-outside-collaborators.ps1
+++ github/github-get-outside-collaborators.ps1
@@ -7,44 +7,32 @@ else {
     Exit
 }
 
-$allOutsideCollabs
-
+#$allOutsideCollabs
+$allEquinorRepos
+<#
 for ($i=1; $i -lt 5; $i++)
 {
     $api = "https://api.github.com/orgs/equinor/outside_collaborators?q&page=$i&per_page=100"
     $collabs = Invoke-WebRequest -Uri $api -Headers @{Authorization="Token $token"}
     $allOutsideCollabs += $collabs.Content | ConvertFrom-Json | select login, html_url
+}#>
+
+for ($i=1; $i -lt 3; $i++)
+{
+    $api = "https://api.github.com/orgs/equinor/repos?q&page=$i&per_page=3"
+    $repos = Invoke-WebRequest -Uri $api -Headers @{Authorization="Token $token"}
+    $allEquinorRepos += $repos.Content | ConvertFrom-Json | select name, html_url
 }
 
-$allOutsideCollabs
+for ($i=1; $i -lt $allEquinorRepos.Length; $i++)
+{
+    $api = "https://api.github.com/repos/equinor/$($allEquinorRepos[$i].name)/collaborators?q&affiliation=outside"
 
-$allDirectPermissionsForOrg = 
+    $response = Invoke-WebRequest -Uri $api -Headers @{Authorization="Token $token"}
 
-$bodyA = '{"query":"query { organization(login: \"Equinor\"){ membersWithRole(first: 100 after: \"$offset\") { edges { node {name, login, email} } pageInfo {endCursor, hasNextPage} } } }"}'
-
-'{
-    organization(login: "Equinor-test") {
-      repositories(first: 100) {
-        edges {
-          node {
-            collaborators {
-              edges {
-                node {
-                  login
-                }
-                permissionSources {
-                  source {
-                    ... on Repository {
-                      REPO: name
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+    if ($response.Content -notlike '`[]')
+    {
+        Write-Host "REPO: $($allEquinorRepos[$i].html_url) has outside collaborators:" -ForegroundColor Yellow
+        $response.Content | ConvertFrom-Json | select login, html_url, permissions | out-default
     }
-  }
-  '
-  
+}
