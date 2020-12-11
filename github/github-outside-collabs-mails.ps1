22fbea0a48eba652b2861e405083031e4612af0d
Audun Lønset Solemdal <AULS@equinor.com>
2020-12-11 12:04:06 +0100

    Splitting necessary for multiple mail receivers

--- github/github-outside-collabs-mails.ps1
+++ github/github-outside-collabs-mails.ps1
@@ -13,7 +13,7 @@ else
 
 $userFrom = "gm_toolbox@equinor.com"
 $excelFile = "C:\appl\outsidecollabs.xlsx"
-$list = Import-Excel -Path $excelFile
+$list = Import-Excel -Path $excelFile -WorksheetName Sheet2
 
 if ($?)
 
@@ -35,15 +35,16 @@ for ($i = 0; $i -lt $emails.Length; $i++) {
 
     if ($emails[$i] -ne $null -and $emails[$i] -ne "")
     {
-        Send-MailMessage -From "GM IT Toolbox <gm_toolbox@equinor.com>" -To $emails[$i] -Subject "Action required for your Github account $($users[$i])" `
+        Send-MailMessage -From "GM IT Toolbox <gm_toolbox@equinor.com>" -To $emails[$i].Split(", ") -Subject "Quarterly access review of repo: $($list.RepoName[$i])" `
         -Cc $userFrom -Body "
+
+        Dette er en test - Audun
 
         This is a quarterly review of repo permissions for outside collaborators to Github repos.
         You are receiving this email because you have admin permissions to the repo: $($list.RepoName[$i]) which gives access to outside collaborators.
 
-        The repo currently has the following outside collaborators: 
+        The repo currently has the following outside collaborators with permissions: 
         $($list.OutsideCollaborators[$i])
-        with the repo permissions: 
         $($list.CollabsPermissions[$i])
 
         Please review the access settings at $($list.RepoSettingsUrl[$i])
