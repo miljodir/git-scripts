877bafd4046f73fcb879c0a90cf7e1cfde4ef46d
audunsolemdal <auls@equinor.com>
2020-12-21 12:29:56 +0100

    Improve ADO cleaner script.

--- ADO-cleaner.ps1
+++ ADO-cleaner.ps1
@@ -9,8 +9,7 @@ else {
 
 # List all ADO orgs
 
-$csv = Import-Csv -Path "C:\maps\orgs2.csv"
-$orgs = ($csv | select "Organization Name")."Organization Name"
+$csv = Import-Csv -Path "C:\maps\orgs2.csv" | sort-object Url
 
 $emptyOrgs = @()
 $populatedOrgs = @()
@@ -18,24 +17,24 @@ $globalProjects = @()
 
 # Find all ADO orgs without projects (no meaningful data what so ever)
 
-foreach ($org in $orgs)
+foreach ($org in $csv)
 {
-    Set-VSTeamAccount -Account $org -PersonalAccessToken $token
+    Set-VSTeamAccount -Account $org."Organization Name" -PersonalAccessToken $token
     $localProjects = Get-VSTeamProject
 
     if (!$localProjects)
     {
-        echo "Org $($org) does not contain a project and should be removed"
-        $emptyOrgs += $org
+        echo "Org $($org.'Organization Name') does not contain a project and should be removed"
+        $emptyOrgs += $org.Url + "_settings/organizationOverview"
     }
     else {
-        echo "Org $($org) contains one or more projects and must be further reviewed before removal."
-        $populatedOrgs += $org
-        $globalProjects += $org + "/" + $localProjects 
+        echo "Org $($org.'Organization Name') contains one or more projects and must be further reviewed before removal."
+        $populatedOrgs += $org.'Organization Name'
+        $globalProjects += ($org.Url + $localProjects)
     }
 }
 
-echo "Here are the following orgs with projects:" -
+echo "Here are the following orgs with projects:"
 $globalProjects
 
 echo ""
