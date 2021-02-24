e4ede774fc87c763bc0f9ba2e30d435bc1bff6c6
audunsolemdal <auls@equinor.com>
2021-02-24 16:28:51 +0100

    ADO: improve outline

--- ADO-statoil-detecter.ps1
+++ ADO-statoil-detecter.ps1
@@ -2,24 +2,26 @@ $ErrorActionPreference = "Continue"
 
 # List all ADO orgs
 
-$csv = Import-Csv -Path "C:\maps\orgs.csv" | sort-object Url
+$csv = Import-Csv -Path "C:\maps\orgs2.csv" | sort-object Url
 
 $collection = @()
+$400DaysAgo = [DateTime]::UtcNow.AddDays(-400)
 
 foreach ($org in $csv)
 {
-    echo "Checking org:" $org.'Organization Name'
+    echo "Checking org:" $org.Url
     $users = az devops user list --org $org.Url | ConvertFrom-Json | select items
 
     foreach ($user in $users.items)
     {
-        if ($user.user.principalName.EndsWith("@statoil.com"))
+        if ($user.user.principalName.Contains("@statoil.") -And $user.lastAccessedDate -lt $400DaysAgo) # Year 2018 is less than 2019 (400 days ago), therefore the logic is correct
         {
             $collection += [pscustomobject] @{
-                OrgName = $org.'Organization Name'
+                OrgName = $org.Url
                 UserName = $user.user.principalName
             }
-            az devops user remove --user $user.user.principalName --org $org.Url -y
+            # echo "Removing user $user.user.principalName from org: $org.Url.."
+            #az devops user remove --user $user.user.principalName --org $org.Url -y
         }
     }
 }
