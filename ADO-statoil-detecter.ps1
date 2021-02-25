6c5d8a5bc57230edd4efa3b02ae06c8a8448ef30
audunsolemdal <auls@equinor.com>
2021-02-25 08:43:07 +0100

    Only log removed users

--- ADO-statoil-detecter.ps1
+++ ADO-statoil-detecter.ps1
@@ -2,26 +2,30 @@ $ErrorActionPreference = "Continue"
 
 # List all ADO orgs
 
-$csv = Import-Csv -Path "C:\maps\orgs3.csv" | sort-object Url
+$csv = Import-Csv -Path "C:\maps\orgsv2.csv" | sort-object Url
 
 $collection = @()
-$400DaysAgo = [DateTime]::UtcNow.AddDays(-400)
+$365DaysAgo = [DateTime]::UtcNow.AddDays(-365)
 
 foreach ($org in $csv)
 {
     echo "Checking org:" $org.Url
-    $users = az devops user list --top 1400 --org $org.Url | ConvertFrom-Json | select items
+    $users = az devops user list --top 1365 --org $org.Url | ConvertFrom-Json | select items
 
     foreach ($user in $users.items)
     {
-        if ($user.user.principalName.EndsWith("@statoil.com") -And $user.lastAccessedDate -lt $400DaysAgo) # Year 2018 is less than 2019 (400 days ago), therefore the logic is correct
+        if ($user.user.principalName.EndsWith("@statoil.com") -And $user.lastAccessedDate -lt $365DaysAgo) # Year 2018 is less than 2019 (365 days ago), therefore the logic is correct
         {
-            $collection += [pscustomobject] @{
-                OrgName = $org.Url
-                UserName = $user.user.principalName
-            }
             echo "Removing user $($user.user.principalName) from org: $($org.Url).."
-            #az devops user remove --user $($user.user.principalName) --org $($org.Url) -y
+            az devops user remove --user $($user.user.principalName) --org $($org.Url) -y
+            if ($?)
+            {
+                echo "Successfully removed $($user.user.principalName) from org: $($org.Url).."
+                $collection += [pscustomobject] @{
+                    OrgName = $org.Url
+                    UserName = $user.user.principalName
+                }
+            }
         }
     }
 }
