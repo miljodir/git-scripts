3eb6b38e1d6eb662a9ae4f0aff41b7d6a6c1cb08
audunsolemdal <audun@solom.no>
2023-04-27 19:16:58 +0200

    Add python script which works for saml

--- github/github-get-members.ps1
+++ github/github-get-members.ps1
@@ -1,46 +1,43 @@
 $url = "https://api.github.com/graphql"
 
-if ($env:github_token)
-{
-    $token = $env:github_token
+if ($env:github_token) {
+  $token = $env:github_token
 }
 else {
-    Write-Host "You must set the 'github_token' env variable!" -ForegroundColor Red
-    Exit
+  Write-Host "You must set the 'github_token' env variable!" -ForegroundColor Red
+  Exit
 }
 
 $offset2 = ""
 
-$bodyB = '{"query":"query { organization(login: \"Equinor\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100) { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
-
-[array]$fullTable = New-Object PsObject -Property @{ login='' ; name='' }
+#$bodyB = '{"query":"query { organization(login: \"Miljodir\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100) { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
+$bodyB = '{"query":"query { organization(login: \"Miljodir\") }"'
+[array]$fullTable = New-Object PsObject -Property @{ login = '' ; name = '' }
 [array]$fullLogin = New-Object PsObject -Property @{ nameId = '' }
 
 $collection = New-Object 'system.collections.generic.dictionary[string,string]'
 
-Do
-{
-    $resp2 = Invoke-RestMethod -Uri $url -Method Post -Body $bodyB -Headers @{Authorization="Token $token"}
+Do {
+  $resp2 = Invoke-RestMethod -Uri $url -Method Post -Body $bodyB -Headers @{Authorization = "Token $token" }
 
-    $offset2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.pageInfo.endCursor
-    $hasNextPage2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.pageInfo.hasNextPage
-    $hashtable2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.edges.node
+  $offset2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.pageInfo.endCursor
+  $hasNextPage2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.pageInfo.hasNextPage
+  $hashtable2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.edges.node
 
-    $bodyB = '{"query":"query { organization(login: \"Equinor\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100 after: \"$offset2\") { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
-    $bodyB = $ExecutionContext.InvokeCommand.ExpandString($bodyB)
+  $bodyB = '{"query":"query { organization(login: \"Miljodir\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100 after: \"$offset2\") { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
+  $bodyB = $ExecutionContext.InvokeCommand.ExpandString($bodyB)
 
-    echo "Has next page: $hasNextPage2"
+  echo "Has next page: $hasNextPage2"
 
-    echo "offset: $offset2"
-    $fullTable += $hashtable2.user
-    $fullLogin += $hashtable2.samlIdentity
+  echo "offset: $offset2"
+  $fullTable += $hashtable2.user
+  $fullLogin += $hashtable2.samlIdentity
 
-    echo "SAML Page completed. Next iteration: "
+  echo "SAML Page completed. Next iteration: "
 
 } While ($hasNextPage2) 
 
-for ($i = 0; $i -lt $fullTable.Length; $i++)
-{
+for ($i = 0; $i -lt $fullTable.Length; $i++) {
   $collection += [pscustomobject] @{
     GithubLogin  = $fullTable[$i].login
     SamlIdentity = $fullLogin[$i].nameId
