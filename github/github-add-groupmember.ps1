c6add72ac7a4fa3d1772495e86d8fd964e95ad91
audunsolemdal <audun@solom.no>
2023-05-15 12:13:05 +0200

    Add script to fetch saml users

--- github/github-add-groupmember.ps1
+++ github/github-add-groupmember.ps1
@@ -1,11 +1,12 @@
-$group = Get-AzADgroup -DisplayName "x"
+$group = Get-AzADgroup -DisplayName "github-tilgang-miljodir"
 
 $sheet = Import-Excel -Path "c:/appl/users.xlsx"
 
 foreach ($user in $sheet) {
     if ($user.mdir_bruker -ne "" -and $null -ne $user.mdir_bruker -and $user.mdir_bruker -ne "?" ) {
-        Write-Host "Login $($user.login) with presumed AAD account $($user.mdir_bruker) will be added" -ForegroundColor Green
-        #Add-AzADGroupMember -TargetGroupObjectId $group.Id -MemberUserPrincipalName $user.mdir_bruker
+        #Write-Host "Login $($user.login) with presumed AAD account $($user.mdir_bruker) will be added" -ForegroundColor Green
+        #$ad = Get-AzADUser -UserPrincipalName $user.mdir_bruker
+        Add-AzADGroupMember -TargetGroupObjectId $group.Id -MemberUserPrincipalName $user.mdir_bruker #-MemberObjectId $ad.Id
     }
     else {
         Write-Host "User $($user.login) will not be added to group" -ForegroundColor Red
