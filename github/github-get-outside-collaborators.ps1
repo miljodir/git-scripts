880da5534178cd1d22800662c1ffd5cdc6b44975
audunsolemdal <audun@solom.no>
2024-02-22 09:42:00 +0100

    Update script

--- github/github-get-outside-collaborators.ps1
+++ github/github-get-outside-collaborators.ps1
@@ -1,46 +1,46 @@
-if ($env:github_token)
-{
-    $token = $env:github_token
-}
-else {
-    Write-Host "You must set the 'github_token' env variable!" -ForegroundColor Red
-    Exit
-}
+$token = gh auth token
 
-$allEquinorRepos
+# if ($env:github_token) {
+#     $token = $env:github_token
+# }
+# else {
+#     Write-Host "You must set the 'github_token' env variable!" -ForegroundColor Red
+#     Exit
+# }
+
+$allorgrepos
 
 # Fetch org admins so we can filter them out from repo admins later on
-$orgAdmins = Invoke-RestMethod -Method Get -Uri "https://api.github.com/orgs/equinor/members?role=admin" -Headers @{Authorization="Token $token"} -ContentType "application/json"
+$orgAdmins = Invoke-RestMethod -Method Get -Uri "https://api.github.com/orgs/miljodir/members?role=admin" -Headers @{Authorization = "Token $token" } -ContentType "application/json"
 
 $url = "https://api.github.com/graphql"
 $offset = ""
-[array]$fulltable = New-Object PsObject -Property @{name=''; collaborators='' }
+[array]$fulltable = New-Object PsObject -Property @{name = ''; collaborators = '' }
 
 # Fetch all repos, get all outside collabs
 $body = @'
-{"query":"query { organization(login: \"Equinor\") { repositories(first: 100) { edges { node { name collaborators(affiliation: OUTSIDE) { edges { permission node {login}}}}} pageInfo {endCursor, hasNextPage}}}}"}
+{"query":"query { organization(login: \"miljodir\") { repositories(first: 100) { edges { node { name collaborators(affiliation: OUTSIDE) { edges { permission node {login}}}}} pageInfo {endCursor, hasNextPage}}}}"}
 '@
 
-Do
-{
-    $response = Invoke-RestMethod -Uri $url -Method Post -Body $body -Headers @{Authorization="Token $token"} -ContentType "application/json"
+Do {
+    $response = Invoke-RestMethod -Uri $url -Method Post -Body $body -Headers @{Authorization = "Token $token" } -ContentType "application/json"
 
     $offset = $response.data.organization.repositories.pageInfo.endCursor
     $hasNextPage = $response.data.organization.repositories.pageInfo.hasNextPage
     $hashtable = $response.data.organization.repositories.edges.node
 
-$body = @'
-    {"query":"query { organization(login: \"Equinor\") { repositories(first: 100 after: \"$offset\") { edges { node { name collaborators(affiliation: OUTSIDE) { edges { permission node {login}}}}} pageInfo {endCursor, hasNextPage}}}}"}
+    $body = @'
+    {"query":"query { organization(login: \"miljodir\") { repositories(first: 100 after: \"$offset\") { edges { node { name collaborators(affiliation: OUTSIDE) { edges { permission node {login}}}}} pageInfo {endCursor, hasNextPage}}}}"}
 '@
 
     $body = $ExecutionContext.InvokeCommand.ExpandString($body)
 
-    echo "Has next page: $hasNextPage"
-    echo "offset: $offset"
+    Write-Host "Has next page: $hasNextPage"
+    Write-Host "offset: $offset"
 
     $fulltable += $hashtable
 
-    echo "Member Page completed. Next iteration: "
+    Write-Host "Member Page completed. Next iteration: "
 
 } While ($hasNextPage) 
 
@@ -51,50 +51,46 @@ $collection = @()
 ## This part fetches saml identities so we can get emails of the repo admins (first all github members)
 $offset2 = ""
 
-$bodyB = '{"query":"query { organization(login: \"Equinor\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100) { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
+$bodyB = '{"query":"query { organization(login: \"miljodir\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100) { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
 
-[array]$fullTable2 = New-Object PsObject -Property @{ login='' ; name='' }
+[array]$fullTable2 = New-Object PsObject -Property @{ login = '' ; name = '' }
 [array]$fullLogin = New-Object PsObject -Property @{ nameId = '' }
 
 $collection2 = @()
 
-Do
-{
-    $resp2 = Invoke-RestMethod -Uri $url -Method Post -Body $bodyB -Headers @{Authorization="Token $token"}
+Do {
+    $resp2 = Invoke-RestMethod -Uri $url -Method Post -Body $bodyB -Headers @{Authorization = "Token $token" }
 
     $offset2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.pageInfo.endCursor
     $hasNextPage2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.pageInfo.hasNextPage
     $hashtable2 = $resp2.data.organization.samlIdentityProvider.externalIdentities.edges.node
 
-    $bodyB = '{"query":"query { organization(login: \"Equinor\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100 after: \"$offset2\") { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
+    $bodyB = '{"query":"query { organization(login: \"miljodir\") { samlIdentityProvider { ssoUrl, externalIdentities(first: 100 after: \"$offset2\") { edges { node { samlIdentity {nameId}, user {name, login} } } pageInfo {endCursor, hasNextPage}  } } } }"}'
     $bodyB = $ExecutionContext.InvokeCommand.ExpandString($bodyB)
 
-    echo "Has next page: $hasNextPage2"
+    Write-Host "Has next page: $hasNextPage2"
 
-    echo "offset: $offset2"
+    Write-Host "offset: $offset2"
     $fullTable2 += $hashtable2.user
     $fullLogin += $hashtable2.samlIdentity
 
-    echo "SAML Page completed. Next iteration: "
+    Write-Host "SAML Page completed. Next iteration: "
 
 } While ($hasNextPage2) 
 
-for ($i = 0; $i -lt $fullTable2.Length; $i++)
-{
-  $collection2 += [pscustomobject] @{
-    FullName  = $fullTable2[$i].name
-    GithubLogin  = $fullTable2[$i].login
-    SamlIdentity = $fullLogin[$i].nameId
-  }
+for ($i = 0; $i -lt $fullTable2.Length; $i++) {
+    $collection2 += [pscustomobject] @{
+        FullName     = $fullTable2[$i].name
+        GithubLogin  = $fullTable2[$i].login
+        SamlIdentity = $fullLogin[$i].nameId
+    }
 }
 
 # Summaraize outside collabs + permissions and repo names into a table.
-foreach ($item in $fulltable)
-{
-    if ($item.collaborators.edges.node.login -ne $null)
-    {
+foreach ($item in $fulltable) {
+    if ($null -ne $item.collaborators.edges.node.login) {
         $collection += [pscustomobject] @{
-            RepoName   = "equinor/$($item.name)"
+            RepoName             = "miljodir/$($item.name)"
             OutsideCollaborators = ($item.collaborators.edges.node.login -join ', ')
             CollabsPermissions   = ($item.collaborators.edges.permission -join ', ')
         }
@@ -102,39 +98,33 @@ foreach ($item in $fulltable)
 }
 
 # Get the repo admins
-foreach ($repo in $collection)
-{
+foreach ($repo in $collection) {
     $api2 = $apiBase + "repos/$($repo.RepoName)/collaborators" #GET /collabs of repo with permissions
-    $call = Invoke-RestMethod -Method Get -Uri $api2 -Headers @{Authorization="Token $token"}
+    $call = Invoke-RestMethod -Method Get -Uri $api2 -Headers @{Authorization = "Token $token" }
 
     $admins = ""
     $adminEmails = ""
 
-        foreach ($user in $call)
-        {
-            if ($user.login -notin $orgAdmins.login -and $user.permissions.admin)
-            {
-                foreach ($id in $collection2)
-                {
-                    if ($user.login -eq $id.GithubLogin)
-                    {
-                        $adminEmails += $id.SamlIdentity + ", "
-                        break
-                    }
+    foreach ($user in $call) {
+        if ($user.login -notin $orgAdmins.login -and $user.permissions.admin) {
+            foreach ($id in $collection2) {
+                if ($user.login -eq $id.GithubLogin) {
+                    $adminEmails += $id.SamlIdentity + ", "
+                    break
                 }
-                
-                $admins += $user.login + ", "
             }
+                
+            $admins += $user.login + ", "
         }
+    }
 
-        if ($adminEmails)
-        {
-            $adminEmails = $adminEmails.Substring(0, $adminEmails.length-2) #Remove last comma and space
-        }
+    if ($adminEmails) {
+        $adminEmails = $adminEmails.Substring(0, $adminEmails.length - 2) #Remove last comma and space
+    }
 
-        $repo | Add-Member -NotePropertyName "RepoAdmins" -NotePropertyValue $admins
-        $repo | Add-Member -NotePropertyName "RepoAdminsEmail" -NotePropertyValue $adminEmails   
-        $repo | Add-Member -NotePropertyName "RepoSettingsUrl" -NotePropertyValue "https://github.com/$($repo.RepoName)/settings/access"
+    $repo | Add-Member -NotePropertyName "RepoAdmins" -NotePropertyValue $admins
+    $repo | Add-Member -NotePropertyName "RepoAdminsEmail" -NotePropertyValue $adminEmails   
+    $repo | Add-Member -NotePropertyName "RepoSettingsUrl" -NotePropertyValue "https://github.com/$($repo.RepoName)/settings/access"
 }
 
 $collection | export-excel
