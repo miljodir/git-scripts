67477bccd3f6a1456b8ec3d4e4b15c326dda00dc
audunsolemdal <audun@solom.no>
2024-07-30 07:45:51 +0200

    Update codescanalert script

--- github/Update-CodeScanAlert.ps1
+++ github/Update-CodeScanAlert.ps1
@@ -38,7 +38,26 @@ $ids = @(
 "CKV_TF_2",
 "CKV2_GHA_1",
 "CKV2_K8S_6",
-"CKV_GHA_7"
+"CKV_GHA_7",
+"CKV_K8S_10",
+"CKV_K8S_11",
+"CKV_K8S_43",
+"CKV_K8S_12",
+"CKV_K8S_15",
+"CKV_K8S_31",
+"CKV_AZURE_206",
+"CKV_AZURE_36",
+"CKV2_AZURE_41",
+"CKV_AZURE_39",
+"CKV_K8S_40",
+"CKV2_GIT_1",
+"CKV_K8S_13"
+)
+
+$devOnlyIds = @(
+    "azure-storage-default-action-deny",
+    "azure-keyvault-specify-network-acl",
+    "azure-database-no-public-access",
 )
 
 $alerts = gh api --method GET "/orgs/$org/code-scanning/alerts?state=open" --paginate | ConvertFrom-Json
@@ -46,9 +65,13 @@ $alerts = gh api --method GET "/orgs/$org/code-scanning/alerts?state=open" --pag
     foreach ($alert in $alerts)
     {
 
-        if ( $alert.most_recent_instance.location.path -like "*-values.yaml")
+        if ( $alert.most_recent_instance.location.path -like "*-values.yaml" -or $alert.most_recent_instance.location.path -like "*/kustomization.yaml" )
         {
             gh api --method PATCH "/repos/$org/$($alert.repository.name)/code-scanning/alerts/$($alert.number)" -f "state=dismissed" -f "dismissed_reason=false positive" -f "dismissed_comment=The alert detected a potential misconfig in the -values.yaml file, which only contains overrides and cannot be evaulated by itself"
+        }
+        elseif ( $alert.most_recent_instance.location.path -like "*dev/*.tf" -and $alert.rule.id -in $devOnlyIds)
+        {
+            gh api --method PATCH "/repos/$org/$($alert.repository.name)/code-scanning/alerts/$($alert.number)" -f "state=dismissed" -f "dismissed_reason=won't fix" -f "dismissed_comment=This alert is acceptable in dev environments"
         }
         elseif ($alert.rule.id -in $ids)
         {
