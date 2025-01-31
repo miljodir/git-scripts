a9d2dd6e5a79a9d08181a7df7ea216977858fed8
audunsolemdal <audun@solom.no>
2025-01-31 11:30:37 +0100

    Update script

--- github/github-license.ps1
+++ github/github-license.ps1
@@ -7,7 +7,9 @@
 param (
     [string] [Parameter(Mandatory=$false)] $csvPath = "C:\Users\audun\Downloads\ghas_active_committers_miljodir_2025-01-31T0015.csv",
     [string] [Parameter(Mandatory=$false)] $csvPath2 = "C:\Users\audun\Downloads\export-miljodir-1738311321.csv",
-    [string] [Parameter(Mandatory=$false)] $copilotCsvPath = "C:\Users\audun\Downloads\miljodir-seat-usage-1738311386.csv"
+    [string] [Parameter(Mandatory=$false)] $copilotCsvPath = "C:\Users\audun\Downloads\miljodir-seat-usage-1738311386.csv",
+    [string] [Parameter(Mandatory=$false)] $workspaceId = "xxxx"
+
 )
 
 $csv = import-csv -Path $csvPath
@@ -34,13 +36,6 @@ $copilotUsedByOtherOrgsOnly = $copilotCsv | Where-Object { -not ($userLogins -co
 # Output users which are using copilot but seemingly not pushing code to the org
 $copilotUsedByOtherOrgsOnly
 
-# Output the missing entries
-Write-Host "Found $($nonPushingMembers.Count) members which have not pushed code in the last 3 months."
-$nonPushingMembers | select "GitHub com login", "GitHub com saml name", "Github com name"
-# Output the result
-#Write-Host "$($latestPerUser.Count) users found commiting code in the last 3 months."
-#$latestPerUser | Select-Object 'User login', 'Organization / repository', 'Last pushed date',  'Last pushed email' | Sort-Object 'Last pushed date' |  Format-Table -AutoSize
-
 # Successful Github Logins
 $githubLoginQuery = @"
 SigninLogs
@@ -52,15 +47,20 @@ SigninLogs
 | project UserPrincipalName
 "@
 
-# Define the workspace and resource group
-$workspaceId = "xxx"
-
 # Execute the query
+$allGithubUsers = $usersCsv | select -ExpandProperty "GitHub com saml name"
 $githubLoginsList = (Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $githubLoginQuery).Results | Select-Object -ExpandProperty UserPrincipalName
-$nonPushingNamesList = $nonPushingMembers | Select-Object -ExpandProperty 'GitHub com saml name'
 
-$missingNames = $nonPushingNamesList | Where-Object { -not ($githubLoginsList -contains $_) }
+$missingNames = $allGithubUsers | Where-Object { -not ($githubLoginsList -contains $_) }
 
 # Output the missing names
-Write-Host "The following names have not signed in to Github for at least the past 5 months" -ForegroundColor Red
+Write-Host "The following names have not signed in to the Github org for at least the past 5 months" -ForegroundColor Red
+Write-Host "Note that some users may have signed in to other orgs under the enterprise, and are still requiring a license." -ForegroundColor Yellow
 $missingNames | ForEach-Object { Write-Host $_ }
+
+# Output the missing entries
+Write-Host "Found $($nonPushingMembers.Count) members which have not pushed code in the last 3 months."
+$nonPushingMembers | select "GitHub com login", "GitHub com saml name", "Github com name"
+# Output the result
+Write-Host "$($latestPerUser.Count) users found commiting code in the last 3 months."
+$latestPerUser | Select-Object 'User login', 'Organization / repository', 'Last pushed date',  'Last pushed email' | Sort-Object 'Last pushed date' |  Format-Table -AutoSize
