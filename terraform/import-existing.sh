9c1cfbc160b6312f98582bea43649e645fedc561
audunsolemdal <audun@solom.no>
2025-03-24 21:16:56 +0100

    Add new tf util

---
+++ terraform/import-existing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Generates terraform "import" blocks based on a local terraform state file.
+# Usage: ./import-existing.sh <group_name>
+
+generate_import_block() {
+    local group_name="$1"
+    jq -r --arg name "$group_name" '
+        [.resources[] | select(.type=="azuread_group" and .name==$name)] 
+        | .[] 
+        | "import { to = \(.module).azuread_group_without_members.\($name) id = \"groups/\(.instances[0].attributes.object_id)\"}"
+    ' terraform.tfstate >> import-blocks.tf
+}
+
+generate_import_block "$1"
